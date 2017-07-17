392150b8c2b37550275b47f3001d53ab9f92ccfd
Alex Crist <accrist@gmail.com>
2017-07-17 01:07:39 -0700

    got up to basic template

---
+++ simple_app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/<name>')
+def index(name="Monkey"):
+    return "Hallo from {}".format(name)
+
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    context = {'num1': num1, 'num2': num2}
+    return render_template('add.html', **context)
+    # return render_template('add.html', num1=num1, num2=num2) # also works
+
+app.run(debug=True)
