ebae687f4f74c6193fb7e759d78fcc9ce87f7074
Alex Crist <accrist@gmail.com>
2017-07-17 15:43:43 -0600

    tried to get forms, does not work

--- simple_app.py
+++ simple_app.py
@@ -1,18 +1,33 @@
 from flask import Flask
+from flask import request, redirect, url_for, flash
 from flask import render_template
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+
+users = []
+
+
+class RegistrationForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/<name>')
-def index(name="Monkey"):
+def index(name='noob'):
     return render_template('index.html', name=name)
 
 
-@app.route('/add/<int:num1>/<int:num2>')
-def add(num1, num2):
-    context = {'num1': num1, 'num2': num2}
-    return render_template('add.html', **context)
-    # return render_template('add.html', num1=num1, num2=num2) # also works
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = form.username.data
+        users.append(user)
+        flash('users so far:{}'" ".join(users))
+        return redirect(url_for('/'))
+    return render_template('register.html', form=form)
 
-app.run(debug=True)
+
+app.run(debug=True, port=8889)
